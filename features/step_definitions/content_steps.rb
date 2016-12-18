ae25b920fd005419b19cd4f570177f598f8e6197
Philip <Philip.zdb@gmail.com>
2016-12-18 17:39:07 +0100

    Changes accpt test so they now check article creation at current date instead of static

--- features/step_definitions/content_steps.rb
+++ features/step_definitions/content_steps.rb
@@ -1,3 +1,9 @@
 Then(/^I should see "([^"]*)"$/) do |content|
   expect(page).to have_content content
 end
+
+And(/^I should see "([^"]*)" current date$/) do |content|
+  date = DateTime.now
+  expect(page).to have_content content
+  expect(page).to have_content date.strftime("%Y-%m-%d")
+end
