12f7000e724eb7c3ddfe7cae51573dbbb3a79e43
Philip <Philip.zdb@gmail.com>
2016-12-19 15:47:05 +0100

    Adds updates to comment model fatorygirl, now creates a new article and associates that comment with it

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :comment do
     content "MyString"
     author "MyString"
+    article Article.create(title:"Testing", content:"Testing", author: "Testing")
   end
 end
