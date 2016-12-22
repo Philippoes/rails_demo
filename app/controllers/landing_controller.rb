a2ce193e7a74b07244c76f47fa442b427f8458fc
Philip <Philip.zdb@gmail.com>
2016-12-22 14:08:19 +0100

    Adds so email params are passed in to the author attribute in comment, displays it on index. test passes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -5,7 +5,7 @@ class LandingController < ApplicationController
 
   def article_comment
     article = Article.find_by(id: params[:article])
-    Comment.create(content: params[:comment], author: "Philip", article: article)
+    Comment.create(content: params[:comment], author: params[:email], article: article)
     redirect_to root_path
   end
 end
