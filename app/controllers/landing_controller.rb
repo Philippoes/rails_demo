d4f0ab94f1924d0aad032b96150c2befd8a60e12
Philip <Philip.zdb@gmail.com>
2016-12-23 17:20:38 +0200

    Adds changes to the controller working towards email validation

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,8 +4,20 @@ class LandingController < ApplicationController
   end
 
   def article_comment
+    if validate_email == false
+
+    end
+    else
     article = Article.find_by(id: params[:article])
     Comment.create(content: params[:comment], author: params[:email], article: article)
     redirect_to root_path
+  end
+
+  def validate_email
+    if params[:email] == invalid
+      return false
+    else
+      return true
+    end
   end
 end
