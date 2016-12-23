3ac212f22fb3abe27f2b06104cab4f998887e5c7
Philip <Philip.zdb@gmail.com>
2016-12-23 19:36:51 +0200

    Removes some unnecessary code in the controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,20 +4,8 @@ class LandingController < ApplicationController
   end
 
   def article_comment
-    if validate_email == false
-      redirect_to root_path
-    end
-    else
     article = Article.find_by(id: params[:article])
     Comment.create(content: params[:comment], author: params[:email], article: article)
     redirect_to root_path
-  end
-
-  def validate_email
-    if params[:email] == nil
-      return false
-    else
-      return true
-    end
   end
 end
