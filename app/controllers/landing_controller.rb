bcb0e3be27fcf1039211c9108d1b4521e4e615ea
Philip <Philip.zdb@gmail.com>
2016-12-27 15:09:44 +0200

    Refactors code in landing controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -18,13 +18,12 @@ class LandingController < ApplicationController
   def article_comment
     article = Article.find_by(id: params[:article])
     kommentar = Comment.create(content: params[:comment], email: params[:email], article: article)
+    redirect_to root_path
     if kommentar.errors.any?
-      redirect_to root_path
       kommentar.errors.full_messages.each do |message|
       flash[:notice] = message
       end
     else
-      redirect_to root_path
       flash[:notice] = "Comment posted!"
     end
   end
