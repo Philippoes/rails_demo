88608ae0c736a0a9a27f3a7e9149b4f5250d82fb
Philip <Philip.zdb@gmail.com>
2017-01-05 12:59:03 +0100

    Adds relationship between user model and article model (one-to-many)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,9 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!
+
   def articles
+    @currentUser = User.find_by(id: current_user.id).email
+    Binding.pry
+    @userArticles = Article.find_by(author: @currentUser)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
   validates :author, presence: true
 
   has_many :comments
+  belongs_to :user
 end
