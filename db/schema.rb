a6bfa80f7bda3765ebea899d42ef685f960ebb40
Philip <Philip.zdb@gmail.com>
2017-01-05 13:04:13 +0100

    Removes author table from articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105115622) do
+ActiveRecord::Schema.define(version: 20170105120306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170105115622) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "author"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
