c23ce5beec47934196bde28ebeefe33f3eb2a2d8
Philip <Philip.zdb@gmail.com>
2017-01-05 13:58:46 +0100

    Fixed a problem in user controller, all tests now passes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,6 @@ class UserController < ApplicationController
   before_action :authenticate_user!
 
   def articles
-    @currentUser = User.find_by(id: current_user.id)
-    @userArticles = Article.find_by(user: @currentUser)
+    @userArticles = current_user.articles
   end
 end
