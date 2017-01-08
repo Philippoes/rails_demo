1957f2e94d6d8f9ebc253f280c5db753c8170afa
Philip <Philip.zdb@gmail.com>
2017-01-08 14:30:52 +0100

    Genereates migration to add user to comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,9 @@
 class CommentController < ApplicationController
+  before_action :authenticate_user!
   def create
+    user = User.find_by(id: current_user.id)
     article = Article.find_by(id: params[:article])
-    kommentar = Comment.create(content: params[:comment], email: params[:email], article: article)
+    kommentar = Comment.create(content: params[:comment], email: user, article: article)
     redirect_to root_path
     if kommentar.errors.any?
       kommentar.errors.full_messages.each do |message|
